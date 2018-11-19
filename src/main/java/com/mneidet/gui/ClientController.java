b69cb28646a46ed35880f1931f750f7f5fb706bb
Vadim Opolski <vaopolskij@gmail.com>
2018-11-19 10:27:26 +0300

    Added working html.

--- src/main/java/com/mneidet/gui/ClientController.java
+++ src/main/java/com/mneidet/gui/ClientController.java
@@ -37,4 +37,10 @@ public class ClientController {
     public List<Client> getClientByName(@RequestParam(name = "name") String name) {
         return clientRepository.findByName(name);
     }
+
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
 }
