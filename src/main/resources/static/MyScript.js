09d80f5f7318fa360ace6636bd0132f33fa3eea7
Opolskiy <Opolskiy@bpcbt.com>
2018-11-20 18:10:12 +0300

    Added saving to local storage.

--- src/main/resources/static/MyScript.js
+++ src/main/resources/static/MyScript.js
@@ -1,3 +1,31 @@
+const CLIENTS_NAME = 'clients';
+var STATE = {
+    [CLIENTS_NAME]: [],
+    formName: 'form1',
+};
+
+function getDataFromLocalStorage(taskName) {
+    var tmp = JSON.parse(localStorage.getItem(taskName));
+    return tmp || [];
+}
+
+const initClientContainer = () => {
+    var data = STATE[CLIENTS_NAME] = getDataFromLocalStorage(TASKS_NAME);
+    var keysCount = data.length || 0;
+
+    if(!keysCount) {
+        coreInsertLiToList({task: "На сегодня новых клиентов нет."});
+    } else {
+        for(var i = 0; i < keysCount; i++) {
+            coreInsertLiToList(data[i], i, true);
+        }
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    initClientContainer();
+});
+
 const deleteNode = (event) => {
     let tmp = event.target;
     let nodeToDelete = tmp.parentNode.parentNode;
@@ -53,15 +81,6 @@ const go = () => {
 //    if( res )
 };
 
-const getDataFromForm = () => {
-    const clientIdVk = document.querySelector('#clientIdVk').value.trim();
-    const clientAddingDate = document.querySelector('#clientAddingDate').value.trim();
-    const check = document.querySelector('#isStatusVIP').checked;
-    console.log(clientIdVk + ' ' + clientAddingDate + ' ' + check);
-    const res = {clientIdVk, clientAddingDate, check};
-
-    return res;
-};
 
 document.addEventListener("DOMContentLoaded", function () {
    coreInsertLiToList({task: "На сегодня нет активных задач"})
@@ -88,21 +107,39 @@ const findEmptyItemToList = () => {
     return null;
 };
 
-const coreInsertLiToList = (data, task = false) => {
+function coreInsertLiToList(data, index, task = false) {
+
+//    var list = document.getElementById('task_list');
+//    var newListNode = document.createElement('li');
+//    newListNode.classList.add('list-group-item');
+    if (task) {
+        newListNode.innerHTML = (data.check ? '<i class="text-danger fa fa-exclamation-triangle"></i> &nbsp ' : '') +
+            data.task + '<br /><span class="text-muted"><small>' +
+            data.reminder + '</small></span> ' +
+            '<span data-id="' + index + '" class="delete_ico" onclick="deleteNode(event)"><i class="fa fa-times"></i></span>';
+        newListNode.setAttribute('data-type', 'task');
+    }
+    else {
+        newListNode.innerHTML = data.task;
+        newListNode.setAttribute('data-type', 'empty');
+    }
+    list.appendChild(newListNode);
+}
+
+const coreInsertLiToList = (data, index, task = false) => {
     const list = document.querySelector('#clientList');
     const newListNode = document.createElement('li');
     newListNode.classList.add('list-group-item');
-    let emptyItem = findEmptyItemToList();
-
-    if (emptyItem !== null){
-        emptyAllList();
-    }
+    // let emptyItem = findEmptyItemToList();
+    //
+    // if (emptyItem !== null){
+    //     emptyAllList();
+    // }
 
     if (task) {
         newListNode.innerHTML = (data.check ? '<span class="text-danger text-uppercase"> DANGER </span>>' : '')
             + data.clientIdVk + '<br / ><span class="text-muted"><small>' + data.clientAddingDate + '</span></small>' +
             '<span class="delete_ico" onclick="deleteNode(event)"><i class="fa fa-times"></i></span>>';
-        list.appendChild(newListNode);
 
     } else {
         newListNode.innerHTML = data.task;
@@ -129,6 +166,44 @@ const makeUL = (array) => {
 
     return list;
 };
+
+const getDataFromForm = () => {
+    const clientIdVk = document.querySelector('#clientIdVk').value.trim();
+    const clientAddingDate = document.querySelector('#clientAddingDate').value.trim();
+    const check = document.querySelector('#isStatusVIP').checked;
+    console.log(clientIdVk + ' ' + clientAddingDate + ' ' + check);
+    const res = {clientIdVk, clientAddingDate, check};
+
+    return res;
+};
+
+
+function insertItemToList() {
+    const clientIdVk = document.querySelector('#clientIdVk').value.trim();
+    const clientAddingDate = document.querySelector('#clientAddingDate').value.trim();
+    const check = document.querySelector('#isStatusVIP').checked;
+    var errCounter = 0;
+
+    if( !clientIdVk.length ) {
+//        printErrorHelper('taskData', "Это поле обязательно для ввода - введите имя клиента", true);
+        errCounter++;
+    }
+
+    // var reminder = document.getElementById('reminderData').value;
+    // reminder = reminder.trim();
+    // if( !reminder.length ) {
+    //     printErrorHelper('reminderData', "Дата задачи обязательна для ввода", true);
+    //     errCounter++;
+    // }
+
+    if (errCounter) {
+        return false;
+    }
+    console.log({ clientIdVk, clientAddingDate, check });
+    STATE[CLIENTS_NAME].push({ clientIdVk, clientAddingDate, check });
+    emptyAllList();
+    return true;
+}
 
 const saveData = () => {
     const name = document.querySelector("#claimData").value;
